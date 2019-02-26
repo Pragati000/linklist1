d55e13b674036ebbda30b883b34b23de179975c0
Pragati000 <pragati.goswami_ccv172gla.ac.in>
2019-02-26 01:05:44 -0800

    create constructor for data and node next

--- src/main/SinglyLinkedList.java
+++ src/main/SinglyLinkedList.java
@@ -23,6 +23,12 @@ public class SinglyLinkedList {
 
         public Node(int data) {
             this.data = data;
+            this.next=null;
+        }
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
         }
     }
 }
